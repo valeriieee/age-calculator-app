2a86e3482345216d4446288201227aea81f7ea3d
valeriieee <chinelovalerie98@gmail.com>
2023-09-07 10:37:41 +0100

    modified changes

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const day = document.querySelector('#dte');
-console.log(day);
 const month = document.querySelector('#mnth');
 const year = document.querySelector('#yrs');
 const button = document.querySelector('#btn');
-
-// let isValid = false;
 
 const dayOutput = document.querySelector('.purple3');
 const monthOutput = document.querySelector('.purple2');
@@ -14,20 +11,25 @@ const dayError = document.querySelector('.dayError');
 const monthError = document.querySelector('.monthError');
 const yearError = document.querySelector('.yearError');
 
-const inputRequiredError = 'This Field is Required';
-const inputDayError = 'Must Be a Valid Day';
+const inputRequiredError = 'This field is required';
+const inputDayError = 'Must Be a valid Day';
 const inputMonthError = 'Must Be a Valid Month';
 const inputYearError = 'Must Be a Valid Year';
 
 function checkDay() {
     let value = day.value;
-    if(value == ''){
+    let monthValue = parseInt(month.value);
+    
+    if (value === '') {
         dayError.innerHTML = inputRequiredError;
         return false;
-    } else if(value < 1 || value > 31){
+    }else if (value < 1 || value > 31) {
         dayError.innerHTML = inputDayError;
         return false;
-    } else{
+    } else if ((monthValue === 4 || monthValue === 6 || monthValue === 9 || monthValue === 11) && value > 30) {
+        dayError.innerHTML = inputDayError;
+        return false;
+    }else {
         dayError.innerHTML = '';
         return true;
     }
@@ -36,13 +38,13 @@ function checkDay() {
 
 function checkMonth() {
     let value = month.value;
-    if(value == ''){
+    if (value === '') {
         monthError.innerHTML = inputRequiredError;
         return false;
     } else if(value < 1 || value > 12){
         monthError.innerHTML = inputMonthError;
         return false;
-    } else{
+    } else {
         monthError.innerHTML = '';
         return true;
     }
@@ -51,10 +53,9 @@ function checkMonth() {
 
 function checkYear() {
     let value = year.value;
-    console.log(value);
     let currentYear = new Date().getFullYear();
     console.log(currentYear);
-    if(value == ''){
+    if (value === '') {
         yearError.innerHTML = inputRequiredError;
         return false;
     }else if(value > currentYear || value < 1900){
@@ -66,58 +67,69 @@ function checkYear() {
     }
 }
 
-function calculate(birthday){
+
+function calculate(birthday) {
     let birthdate = new Date(birthday);
     let today = new Date();
-    console.log(birthday);
 
     let years = today.getFullYear() - birthdate.getFullYear();
     let months = today.getMonth() - birthdate.getMonth();
     let days = today.getDate() - birthdate.getDate();
-    console.log(birthdate);
-    console.log(today);
-    console.log(days);
-    console.log(years);
 
-    if(months < 0 || (months === 0 & days < 0)) {
-         years = '--';
-        if (months === 0){
-            months = 11;
-        }else{
-            months = 12 + months;
-        }
-        days = 30 + days;
+    if (days < 0) {
+        // Calculate the days for cases where days is negative
+        const daysInMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+        days += daysInMonth;
+        months--;
     }
-    console.log(years);
+
+    if (months < 0) {
+        // Calculate the months for cases where months is negative
+        months += 12;
+        years--;
+    }
+
     yearOutput.innerHTML = years;
     monthOutput.innerHTML = months;
     dayOutput.innerHTML = days;
 }
 
+// Function to save the birthday to local storage
+function saveBirthday(birthday) {
+    localStorage.setItem('userBirthday', birthday);
+}
+
+// Function to load the saved birthday from local storage
+function loadBirthday() {
+    return localStorage.getItem('userBirthday');
+}
+
+// Function to pre-fill input fields with the loaded birthday
+function prefillInputFields() {
+    const savedBirthday = loadBirthday();
+    if (savedBirthday) {
+        const [savedYear, savedMonth, savedDay] = savedBirthday.split('-');
+        day.value = savedDay;
+        month.value = savedMonth;
+        year.value = savedYear;
+    }
+}
+
+// Add an event listener to pre-fill input fields when the page loads
+window.addEventListener('load', () => {
+    prefillInputFields();
+});
+
 button.addEventListener('click', e => {
     e.preventDefault();
-
     let inputDay = checkDay();
     let inputMonth = checkMonth();
     let inputYear = checkYear();
 
-    console.log(inputDay);
-
-    if(!inputDay||!inputMonth||!inputYear) return
-    let birthday = `${day.value}/${month.value}/${year.value}`;
+    if (!inputDay || !inputMonth || !inputYear) return;
+    let birthday = `${year.value}-${month.value}-${day.value}`;
     calculate(birthday);
-    console.log(calculate);
+
+    // Save the birthday to local storage
+    saveBirthday(birthday);
 });
-
-
-// day.addEventListener('submit', e =>{
-// if (+day.value>31) {
-//     // document.getElementById('dayError').style.display = 'block';
-//     dayError.textContent = 'Must Be A Valid Date';
-//     isValid = false;
-//     return;
-// } else if(){
-    
-// }
-
-// });
